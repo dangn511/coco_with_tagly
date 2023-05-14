77a7412fbe1152ad853a45e18a8dc4dcdfa6f9d3
dangn511 <dangn511@gmail.com>
2023-05-15 07:24:50 +0800

    fixed issue with downloading exports

--- client/src/models/exports.js
+++ client/src/models/exports.js
@@ -9,6 +9,7 @@ export default {
       method: "GET",
       responseType: "blob"
     }).then(response => {
+      console.log(response)
       const url = window.URL.createObjectURL(new Blob([response.data]));
       const link = document.createElement("a");
       link.href = url;
